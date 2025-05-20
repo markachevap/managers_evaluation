b1ba6a6ccf5fede50ec513e0e46b7384ba44a3a2
Markacheva Polina <vuzpolinam@gmail.com>
2025-05-20 17:07:35 +0300

    Update urls.py

--- backend/analytics/urls.py
+++ backend/analytics/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import LeaderAnalyticsView, ManagerAnalyticsView, ComparisonAnalyticsView
+from . import views
 
 app_name = 'analytics'
 
 urlpatterns = [
-    path('leader/', LeaderAnalyticsView.as_view(), name='leader-analytics'),
-    path('manager/', ManagerAnalyticsView.as_view(), name='manager-analytics'),
-    path('compare/', ComparisonAnalyticsView.as_view(), name='comparison-analytics'),
+    path('leader/', views.LeaderAnalyticsView.as_view(), name='leader-analytics'),
+    path('manager/', views.ManagerAnalyticsView.as_view(), name='manager-analytics'),
+    path('compare/', views.ComparisonAnalyticsView.as_view(), name='comparison-analytics'),
+    path('manager/<int:manager_id>/', views.ManagerAnalyticsView.as_view(), name='manager-analytics')
 ]
